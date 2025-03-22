9d3faa4850d32d0f957b6c7501d0c003281ce9c3
gurutze-fagoaga <gurutzefagoaga@alumnos.cei.es>
2025-03-22 16:07:23 +0100

    terminado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+/*---------------- VARIABLES Y CONSTANTES ----------------*/
+
 const body = document.querySelector("#body");
 const listaDeItems = document.querySelectorAll("#navItem-link");
 const listaDeContenedores = document.querySelectorAll(".navItem");
@@ -16,37 +18,65 @@ const listaDeImagenes =
         "./img/cover-deep-state.jpg"
     ]
 
+
+/*---------------- FUNCIONES Y EVENT LISTENERS ----------------*/
+
+
 listaDeItems.forEach((item, id) => {
 
+    // añadir la clase active a todos los items  
     item.classList.add("active");
+
+    /*-------- ESTILOS DE FONDO EN MOUSEOVER" --------*/
 
     function cambiarEstilos()  {
         body.style.backdropFilter= "brightness(0.5)";
         body.style.backgroundImage = `url('${listaDeImagenes[id]}')`;
         body.style.backgroundSize = "cover";
         body.style.transition = "all .8s ease";
-        // body.style.transition = "backgroundImage 0s ease";
     }
-    
+
+
+    /*---------- EVENT LISTENER "MOUSEOVER" --------*/
+
 
     item.addEventListener("mouseover", () => {
-        // item.classList.add("active");
+
+
+
+        // cambiar el fondo al hacer mouseover
         cambiarEstilos();
+
+        // elimino la clase active a todos los items
+
+        listaDeItems.forEach(item => {
+            item.classList.remove("active");
+
+        })
+
+
+        // añadir la clase active al item que me interesa 
+        item.classList.add("active");
+
     })
 
+    /*---------- EVENT LISTENER "MOUSEOUT" --------*/
+
     item.addEventListener("mouseout", () => {
-        // item.classList.add("active");
+        // cambiar fondo al hacer mouseout
         body.style.backdropFilter= "brightness(0)";
         body.style.backgroundImage = `none`;
 
+        // eliminar la clase active al hacer mouseout
+        listaDeItems.forEach(item => {
+            item.classList.add("active");
+        })
+       
+        // transiciones 
+        body.style.transition = "all .8s ease";
+        item.style.transition = "opacity .8s ease";
+
     })
-
-    // item.addEventListener("click" , () =>{
-    //     item.classList.remove("active");
-    //     // item.classList.add("active");
-    // })
-
-    
 
 });
 
